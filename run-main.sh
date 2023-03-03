adde614b4f42cfa4a268ff9e7ecf46ddc3d0b25f
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-03-03 18:28:05 -0500

    updated: run-main.sh

---
+++ run-main.sh
@@ -0,0 +1,63 @@
+
+echo starttime=`date`
+
+targetDIR=`pwd`
+echo targetDIR=${targetDIR}
+
+conda create --clone base --name envTest 
+
+conda install --yes -c conda-forge \
+        earthengine-api \
+        google-cloud-sdk \
+        geemap \
+        rioxarray \
+        earthpy
+
+pip install rHEALPixDGGS
+pip install ease-grid
+pip install dggrid4py
+pip install pydggrid
+
+conda create  --yes -p ${targetDIR}
+conda install --yes -p ${targetDIR} -c conda-forge \
+        "r-arrow" \
+        "r-bayesplot" \
+        "r-Cairo" \
+        "r-EnvStats" \
+        "r-flexdashboard" \
+        "r-gdalutilities" \
+        "r-gdalutils" \
+        "r-gdata" \
+        "r-gdtools" \
+        "r-ggiraph" \
+        "r-ggpubr" \
+        "r-gifski" \
+        "r-jqr" \
+        "r-magick" \
+        "r-pdftools" \
+        "r-pkgdown" \
+        "r-R2jags" \
+        "r-rgdal" \
+        "r-ragg" \
+        "r-raster" \
+        "r-rastervis" \
+        "r-rgl" \
+        "r-rjags" \
+        "r-rJava" \
+        "r-rjson" \
+        "r-RMariaDB" \
+        "r-RMySQL" \
+        "r-rstan" \
+        "r-rstoolbox" \
+        "r-sf" \
+        "r-sp" \
+        "r-systemfonts" \
+        "r-terra" \
+        "r-textshaping" \
+        "r-tidyverse" \
+        "r-tiff" \
+        "r-vdiffr" > stdout.conda-install 2> stderr.conda-install
+
+# conda update --yes -name base conda
+echo finishtime=`date`
+
