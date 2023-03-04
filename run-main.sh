092aeea5c5af50fae0607fa8cb1c8c8e71b3419e
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-03-03 19:42:40 -0500

    updated: run-main.sh

--- run-main.sh
+++ run-main.sh
@@ -21,37 +21,39 @@ pkgsPyPI=( \
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 timeStamp=`date +"%Y%m%d%H%M%S"`
 myEnvName=envGeo${timeStamp}
-
-echo;echo "environment cloning (base to ${myEnvName}) begins: `date`"
-
-conda create --yes --clone base --name ${myEnvName}
-sleep 2
-
-conda activate ${myEnvName}
-sleep 2
-
-echo;echo "environment cloning (base to ${myEnvName}) complete: `date`"
+echo;echo "[`date +"%Y-%m-%d:%H-%M-%S"`]: environment ${myEnvName} build begins"
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-echo;echo conda packages installation begins: `date`
+echo;echo "[`date +"%Y-%m-%d:%H-%M-%S"`]: environment cloning (base to ${myEnvName}) begins"
+
+# conda create --yes --clone base --name ${myEnvName}
+sleep 2
+
+# conda activate ${myEnvName}
+sleep 2
+
+echo;echo "[`date +"%Y-%m-%d:%H-%M-%S"`]: environment cloning (base to ${myEnvName}) complete"
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+echo;echo "[`date +"%Y-%m-%d:%H-%M-%S"`] conda packages installation begins"
 for temppkg in "${pkgsCODA[@]}"
 do
-    echo conda installion begins: `date`: ${temppkg}
+    echo "    [`date +"%Y-%m-%d:%H-%M-%S"`] conda installion begins: ${temppkg}"
     # conda install --yes -c conda-forge ${temppkg} > stdout.conda-install.${temppkg} 2> stderr.conda-install.${temppkg}
-    echo conda installion complete: `date`: ${temppkg}
+    echo "    [`date +"%Y-%m-%d:%H-%M-%S"`] conda installion complete: ${temppkg}"
 done
-echo;echo conda packages installation complete: `date`
+echo "[`date +"%Y-%m-%d:%H-%M-%S"`] conda packages installation complete"
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-echo;echo pypi packages installation begins: `date`
+echo;echo "[`date +"%Y-%m-%d:%H-%M-%S"`]: pypi packages installation begins"
 for temppkg in "${pkgsPyPI[@]}"
 do
-    echo pip installion begins: `date`: ${temppkg}
+    echo "    [`date +"%Y-%m-%d:%H-%M-%S"`] pip installion begins: ${temppkg}"
     # pip install --no-input ${temppkg} > stdout.pip-install.${temppkg} 2> stderr.pip-install.${temppkg}
-    echo pip installion complete: `date`: ${temppkg}
+    echo "    [`date +"%Y-%m-%d:%H-%M-%S"`] pip installion complete: ${temppkg}"
 done
-echo;echo pypi packages installation complete: `date`
+echo "[`date +"%Y-%m-%d:%H-%M-%S"`]: pypi packages installation complete"
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 # conda update --yes -name base conda
-echo finishtime=`date`
+echo;echo "[`date +"%Y-%m-%d:%H-%M-%S"`]: environment ${myEnvName} build complete"
